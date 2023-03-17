0cf798df9b2dc12d9a6b41437c46b91bf5598f15
sgupta1409 <sgupta@qodeleaf.com>
2023-03-17 20:45:19 +0530

    feat :add recursion methods       add object methods       add string methods       add module methods       add function methods       and more

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,19 +8,21 @@ exports = typeof window === 'undefined' ? global : window;
  */
 
 exports.bestPracticesAnswers = {
-  globals: function() {
-    myObject = {
+  globals: function () {
+    var myObject = {
       name: 'Jory'
     };
 
     return myObject;
   },
 
-  parseInt: function(num) {
-    return parseInt(num);
+  parseInt: function (num) {
+   return parseInt(num,10);
   },
 
-  identity: function(val1, val2) {
+  identity: function (val1, val2) {
+
+    return val1 === val2;
 
   }
 };
